ed0528ef56c4ad03c8c70d389b3e57d6af56c9fc
Amit Kumar <amit.kumar4436@gmail.com>
2015-07-31 23:22:21 +0530

    new hello has been added css added going for new functionalities

---
+++ simple-hello.js
@@ -0,0 +1,16 @@
+Tasks = new Mongo.Collection("tasks");
+
+if (Meteor.isClient) {
+  // This code only runs on the client
+  Template.body.helpers({
+    tasks: function() {
+      return Tasks.find({});
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
